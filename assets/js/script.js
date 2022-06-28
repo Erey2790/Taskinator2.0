29a59dd143a42eccaf9db656ef492cbeb046b600
unknown <erey2790@gmail.com>
2022-06-27 21:41:18 -0500

    created task handler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 var buttonEl = document.querySelector('#save-task');
 var taskToDoEl = document.querySelector('#task-to-do');
 
-buttonEl.addEventListener('click', function() {
-    var listItemEl = document.createEl('li');
+var createTaskHandler = function() {
+    var listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
-    listItemEl.textContent = 'This is a new task.';
+    listItemEl.textContent = 'This is a new task!';
     taskToDoEl.appendChild(listItemEl);
-});
+}
+
+buttonEl.addEventListener('click', createTaskHandler)
