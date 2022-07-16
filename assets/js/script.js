00de75862c7020e9a63374d89b3048f3f7835b73
unknown <erey2790@gmail.com>
2022-07-16 14:23:58 -0500

    added json local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,6 +61,7 @@ var createTaskEl = function(taskDataObj) {
   taskDataObj.id = taskIdCounter;
   tasks.push(taskDataObj);
 
+  saveTasks()
   // increase task counter for next unique id
   taskIdCounter++;
 };
@@ -120,6 +121,8 @@ var completeEditTask = function(taskName, taskType, taskId) {
     }
   }
 
+  saveTasks()
+
   alert("Task Updated!");
 
   // remove data attribute from form
@@ -168,6 +171,7 @@ var taskStatusChangeHandler = function(event) {
       tasks[i].status = statusValue;
     }
   }
+  saveTasks()
 };
 
 var editTask = function(taskId) {
@@ -212,7 +216,13 @@ var deleteTask = function(taskId) {
 
   // reassign tasks array to be the same as updatedTaskArr
   tasks = updatedTaskArr;
+
+  saveTasks()
 };
+
+var saveTasks = function() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
